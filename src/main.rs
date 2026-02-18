9484fb3232ca9a244bd7fe6eeedb84cdd10cbc55
kabir-fx <vatshivam49888@gmail.com>
2026-02-18 13:43:06 +0530

    feat: Add tests for all storage types.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ pub trait CustomSerializer<T, E> {
     fn convert_from_bytes(&self, bytes: Vec<u8>) -> Result<T, E>;
 }
 
-#[derive(Debug, PartialEq, BorshSerialize, BorshDeserialize, SchemaRead, SchemaWrite, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, BorshSerialize, BorshDeserialize, SchemaRead, SchemaWrite, Serialize, Deserialize)]
 pub struct Person {
     name: String,
     age: u64,
@@ -35,4 +35,52 @@ fn main() {
     println!("Loaded: {:?}", loaded);
 
     println!("{}", borsh_storage.has_data());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn generate_test_object() -> Person{
+        Person { name: String::from("Marshal"), age: 324 }
+    }
+
+    #[test]
+    fn test_borsh() {
+        let mut borsh_storage = Storage::new(BorshData);
+
+        let person = generate_test_object();
+        borsh_storage.save(person.clone());
+
+        let loaded: Person = borsh_storage.load();
+
+        assert_eq!(person, loaded);
+        assert_eq!(true, borsh_storage.has_data());
+    }
+
+    #[test]
+    fn test_wincode() {
+        let mut wincode_storage = Storage::new(WincodeData);
+
+        let person = generate_test_object();
+        wincode_storage.save(person.clone());
+
+        let loaded: Person = wincode_storage.load();
+
+        assert_eq!(person, loaded);
+        assert_eq!(true, wincode_storage.has_data());
+    }
+
+    #[test]
+    fn test_json() {
+        let mut json_storage = Storage::new(JSONData);
+
+        let person = generate_test_object();
+        json_storage.save(person.clone());
+
+        let loaded: Person = json_storage.load();
+
+        assert_eq!(person, loaded);
+        assert_eq!(true, json_storage.has_data());
+    }
 }
